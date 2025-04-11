b38a1247a3a5f7ea017d7ea64e81949c654887de
Grzegorz-Baczek <134709@student.uwm.edu.pl>
2025-04-11 14:03:08 +0200

    added init in database

---
+++ src/FTS.Infrastructure/AppOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace FTS.Infrastructure;
+
+public sealed class AppOptions
+{
+    public string Name { get; set; }
+}
---
+++ src/FTS.Infrastructure/DAL/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FTS.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace FTS.Infrastructure.DAL.Configurations;
+
+internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasMany(c => c.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
+
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name)
+            .IsRequired();
+    }
+}
