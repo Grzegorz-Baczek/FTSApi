7ee945c6ec3c34f2639acd74c64945f50eb9741a
Grzegorz-Baczek <134709@student.uwm.edu.pl>
2025-05-05 16:16:01 +0200

    Get_categoryId

---
+++ src/FTS.Application/Queries/Categories/Handlers/GetCategoryHandler.cs
@@ -0,0 +1,30 @@
+﻿using FTS.Application.Abstractions;
+using FTS.Application.Exceptions;
+using FTS.Core.Entities;
+using MediatR;
+
+namespace FTS.Application.Queries.Categories.Handlers;
+
+public record GetCategoryQuery(Guid Id) : IRequest<Category>;
+
+internal sealed class GetCategoryHandler : IRequestHandler<GetCategoryQuery, Category>
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public GetCategoryHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<Category> Handle(GetCategoryQuery query, CancellationToken ct)
+    {
+        var category = await _categoryRepository.GetCategoryAsync(query.Id, ct);
+
+        if (category is null)
+        {
+            throw new NotFoundCategoryException(category.Id);
+        }
+
+        return category;
+    }
+}
