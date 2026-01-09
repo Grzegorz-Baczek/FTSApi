bf16ca15c1bfd7b57fced0ed89849dcbe28a0fdd
Grzegorz-Baczek <134709@student.uwm.edu.pl>
2026-01-09 14:50:58 +0100

    create category and add azure

---
+++ src/FTS.App/Components/Pages/Categories/CreateCategory.razor.cs
@@ -0,0 +1,28 @@
+﻿using FTS.App.Components.Pages.Categories.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FTS.App.Components.Pages.Categories;
+
+public partial class CreateCategory
+{
+    [Inject]
+    public CategoryApiClient CategoryApiClient { get; set; } = default!;
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
+    public CategoryViewModel CategoryViewModel = new CategoryViewModel();
+
+    private async Task CreateCategoryAsync()
+    {
+        var createCategory = new CreateCategoryModel(CategoryViewModel.Name);
+        await CategoryApiClient.CreateCategoryAsync(createCategory);
+
+        NavigateToProducts();
+    }
+
+    private void NavigateToProducts()
+    {
+        NavigationManager.NavigateTo("/products");
+    }
+}
+
--- src/FTS.App/Components/Pages/Products/Products.razor.cs
+++ src/FTS.App/Components/Pages/Products/Products.razor.cs
@@ -22,4 +22,9 @@ public partial class Products : ComponentBase
     {
         NavigationManager.NavigateTo("/createproduct");
     }
+
+    private void NavigateCreateCategory()
+    {
+        NavigationManager.NavigateTo("/createcategory");
+    }
 }
