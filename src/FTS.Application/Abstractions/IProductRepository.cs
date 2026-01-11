38bb3893652fe8a94307d5de35bc4ab2d8053268
Grzegorz-Baczek <134709@student.uwm.edu.pl>
2026-01-11 20:32:45 +0100

    delete-backend-product

--- src/FTS.Application/Abstractions/IProductRepository.cs
+++ src/FTS.Application/Abstractions/IProductRepository.cs
@@ -6,5 +6,6 @@ public interface IProductRepository
 {
     Task AddProductAsync(Product product, CancellationToken ct);
     Task<IEnumerable<Product>> GetProductsAsync(CancellationToken ct);
+    Task DeleteProductAsync(Product product, CancellationToken ct);
     Task<Product?> GetProductAsync(Guid id, CancellationToken ct);
 }
