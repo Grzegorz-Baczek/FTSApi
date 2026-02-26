10b3744f93df95b3e600db3589b8b38f0547f68e
Grzegorz-Baczek <134709@student.uwm.edu.pl>
2026-02-26 21:24:04 +0100

    backend

---
+++ src/FTS.Infrastructure/DAL/Handlers/CookbookRecipes/GetCookbookRecipes/GetCookbookRecipesQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using FTS.Application.DTO;
+using FTS.Application.Handlers.CookbookRecipes.Queries.GetCookbookRecipes;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FTS.Infrastructure.DAL.Handlers.CookbookRecipes.GetCookbookRecipes;
+
+internal sealed class GetCookbookRecipesQueryHandler(
+    FTSDbContext dbContext) : IRequestHandler<GetCookbookRecipesQuery, IReadOnlyCollection<CookbookRecipeDto>>
+{
+    public async Task<IReadOnlyCollection<CookbookRecipeDto>> Handle(GetCookbookRecipesQuery query, CancellationToken cancellationToken)
+    {
+        var cookbookRecipes = await dbContext.CookbookRecipes
+            .Where(cbr => cbr.CookbookId == query.CookbookId)
+            .Select(cbr => new CookbookRecipeDto(cbr.Id, cbr.RecipeId, cbr.Recipe.Title, cbr.PinnedAt))
+            .ToListAsync(cancellationToken);
+
+        return cookbookRecipes;
+    }
+}
