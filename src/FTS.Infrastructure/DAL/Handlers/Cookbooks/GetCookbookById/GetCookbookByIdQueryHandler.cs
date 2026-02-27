090fd1deb693978e47ee118397a8a6c6db569489
Grzegorz-Baczek <134709@student.uwm.edu.pl>
2026-02-27 20:38:33 +0100

    handler. exception, controller, query

---
+++ src/FTS.Infrastructure/DAL/Handlers/Cookbooks/GetCookbookById/GetCookbookByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using FTS.Application.Abstractions;
+using FTS.Application.DTO;
+using FTS.Application.Handlers.Cookbooks.Queries.GetCookbookById;
+using FTS.Infrastructure.Exceptions.NotFoundExceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FTS.Infrastructure.DAL.Handlers.Cookbooks.GetCookbookById;
+
+internal sealed class GetCookbookByIdQueryHandler(
+    FTSDbContext dbContext,
+    IUserRepository userRepository) : IRequestHandler<GetCookbookByIdQuery, CookbookDto>
+{
+    public async Task<CookbookDto> Handle(GetCookbookByIdQuery query, CancellationToken cancellationToken)
+    {
+        var userId = userRepository.GetUserId();
+
+        var cookbook = await dbContext.Cookbooks
+            .Where(cb => cb.Id == query.Id && cb.UserId == userId)
+            .Select(cb => new CookbookDto(cb.Id, cb.Name))
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (cookbook is null)
+        {
+            throw new NotFoundCookbookException(query.Id);
+        }
+
+        return cookbook;
+    }
+}
---
+++ src/FTS.Infrastructure/Exceptions/NotFoundExceptions/NotFoundCookbookException.cs
@@ -0,0 +1,13 @@
+﻿using FTS.Core.Exceptions;
+
+namespace FTS.Infrastructure.Exceptions.NotFoundExceptions;
+
+internal class NotFoundCookbookException : CustomException
+{
+    public Guid Id { get; }
+
+    public NotFoundCookbookException(Guid id) : base($"CookbookId: {id} not found")
+    {
+        Id = id;
+    }
+}
